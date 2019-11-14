7345c512b12a758fca88a8d49450217736c84c8a
chenyang <chenyang@Koolearn.com>
2019-11-14 20:24:49 +0800

    little revise

--- javaStore/src/main/java/com/learning/java/store/performanceBenchMark/MappedByteBufferNio.java
+++ javaStore/src/main/java/com/learning/java/store/performanceBenchMark/MappedByteBufferNio.java
@@ -22,36 +22,41 @@ public class MappedByteBufferNio {
     MappedByteBuffer mappedByteBuffer;
 
     public MappedByteBufferNio() throws Exception{
-        this.aFile = new RandomAccessFile("/Users/chenyang/data/nio-data.txt", "rw");
-
+//        this.aFile = new RandomAccessFile("/Users/chenyang/data/nio-data.txt", "rw");
+        this.aFile = new RandomAccessFile("D:\\data\\message", "rw");
         this.inChannel = aFile.getChannel();
         //将文件的前 100*1024*1024 个字节映射到内存中
-        this.mappedByteBuffer=inChannel.map(FileChannel.MapMode.READ_WRITE,0,100*1024*1024);
+        this.mappedByteBuffer=inChannel.map(FileChannel.MapMode.READ_WRITE,0,1*1024*1024);
+        mappedByteBuffer.position();
     }
 
     public static void main(String[] args) throws Exception{
         MappedByteBufferNio mappedByteBufferNio=new MappedByteBufferNio();
 
-        long start= System.currentTimeMillis();
-        for(int k=0;k<100;k++) {//100*1024*1024=100m
-            System.out.println(k);
-            for (int i = 0; i < 1024; i++) {//1024*1024=1m
-                for (int j = 0; j < 32; j++) {//1024
+//        long start= System.currentTimeMillis();
+//        for(int k=0;k<100;k++) {//100*1024*1024=100m
+//            System.out.println(k);
+//            for (int i = 0; i < 1024; i++) {//1024*1024=1m
+//                for (int j = 0; j < 32; j++) {//1024
                     mappedByteBufferNio.writeFile();
-                }
-            }
-        }
-        System.out.println("执行时间:"+(System.currentTimeMillis()-start)/1000);
+
+//                }
+//            }
+//        }
+//        System.out.println("执行时间:"+(System.currentTimeMillis()-start)/1000);
         mappedByteBufferNio.inChannel.close();
         mappedByteBufferNio.aFile.close();
     }
 
 
     private void writeFile() throws Exception{
-        inChannel.position(aFile.length());
         String newData = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" ;//32 byte
         byte[] bytes=newData.getBytes();
         mappedByteBuffer.put(bytes);
+        inChannel.write(mappedByteBuffer);
 
+//        System.out.println(mappedByteBuffer.capacity());
+//        System.out.println(mappedByteBuffer.limit());
+//        System.out.println(mappedByteBuffer.position());
     }
 }
